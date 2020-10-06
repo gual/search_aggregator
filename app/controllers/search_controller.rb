58ca7e477d2734495fab3750213a1f14c7ee8991
Gual Escalante <gual23@gmail.com>
2020-10-05 22:51:37 -0600

    Added engine params functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,19 @@ class SearchController < ApplicationController
   def search
     params.require([:engine, :text])
     params.permit(:engine, :text)
-    engine = params[:engine]
+    engine = params[:engine].downcase
     text = params[:text]
-    @message = 'Search result'
+
+    case engine
+    when 'google'
+      @message = 'Google search'
+    when 'bing'
+      @message = 'Bing search'
+    # Added 'all' in case more engines are added and 'both' becomes syntactically incorrect
+    when 'both', 'all'
+      @message = 'Aggregated search'
+    else
+      raise "Invalid engine"
+    end
   end
 end
