f71012c05c4559078d1cf865cf924ece1f9b2d87
Gual Escalante <gual23@gmail.com>
2020-10-05 22:11:37 -0600

    Added params to search endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def search
+    params.require([:engine, :text])
+    params.permit(:engine, :text)
+    engine = params[:engine]
+    text = params[:text]
     @message = 'Search result'
   end
 end
